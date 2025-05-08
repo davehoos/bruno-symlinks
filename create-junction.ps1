0cd274af3d654aa7048d84c06a3dd7283004c9b1
Dave Hooson <dave@hoosonconsultancy.co.uk>
2025-05-08 15:36:02 +0100

    Updates

--- create-junction.ps1
+++ create-junction.ps1
@@ -7,6 +7,6 @@ $sourceFile = "PROD.bru"
 #Create Junction - Only supported for folders
 ##New-Item -Path $linkFile -ItemType Junction -Value $sourceFile
 
-##New-Item -Path $linkFile -ItemType SymbolicLink -Value $sourceFile
+New-Item -Path $linkFile -ItemType SymbolicLink -Value $sourceFile
 
-Remove-Item -Path $linkFile
+#Remove-Item -Path $linkFile
