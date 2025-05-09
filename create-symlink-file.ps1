b9dc3ca4c9ebe2212cc5a3d3b271e1c399d05fe2
Dave Hooson <dave@hoosonconsultancy.co.uk>
2025-05-09 08:06:47 +0100

    Updated Symbolic Link

--- create-symlink-file.ps1
+++ create-symlink-file.ps1
@@ -1,13 +1,21 @@
 #Link File
 $linkFolder = "bruno\API Test\environments" 
 
-$linkFile = $linkFolder + "\PROD.bru"
+$linkFile = "PROD.bru"
+
+# Store the current directory in a variable
+$currentDirectory = Get-Location
+
+# Change current directory to location of symbolic link creation
+Set-Location -Path $linkFolder
 
 #Source File
-$sourceFile = "environments\PROD.bru"
+$sourceFile = "..\..\..\environments\PROD.bru"
 
 #Create Environment Folder if doesn't exist
 New-Item -Path $linkFolder -ItemType Directory -ErrorAction SilentlyContinue
 
 #Create Symbolic Link
 New-Item -Path $linkFile -ItemType SymbolicLink -Value $sourceFile
+
+Set-Location -Path $currentDirectory
